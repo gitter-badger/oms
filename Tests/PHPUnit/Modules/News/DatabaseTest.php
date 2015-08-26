092aa2e97bd192bb0f99ac3180f6519a72885ee4
Dennis Eichhorn <spl1nes.com@googlemail.com>
2015-08-26 19:57:43 +0200

    Potential tests

---
+++ Tests/PHPUnit/Modules/News/DatabaseTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests\PHPUnit\Modules\News;
+
+require_once __DIR__ . '/../../../phpOMS/Autoloader.php';
+require_once __DIR__ . '/../../../config.php';
+
+class DatabaseTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp() 
+	{
+
+	}
+
+    public function testBasicFunctionallity()
+    {
+    }
+}
